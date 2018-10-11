7a9785427f04186ba6ded91d5d778406192b93ed
Dimanche Jhon Steeve <jsteeve@noukod.com>
2018-10-11 17:01:08 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,6 +6,55 @@ class Song
     @@all
   end
 
+  def self.create
+    this = self.new
+    this.save
+    this
+  end
+
+  def self.new_by_name(name = "")
+    this = self.new
+    this.name =  name
+    this
+  end
+
+  def self.create_by_name(name = "")
+    this = self.create
+    this.name = name
+    this
+  end
+
+  def self.find_by_name(name = "")
+    this = self.all.find{|s| s.name == name}
+  end
+
+  def self.find_or_create_by_name(name = "")
+    this = self.new
+    if (this = self.find_by_name(name)).nil?
+      this = self.create_by_name(name)
+    end
+    this
+  end
+
+  def self.alphabetical
+    self.all.sort{|s1, s2| s1.name <=> s2.name}
+  end
+
+  def self.new_from_filename(filename = "")
+    this = self.new
+    this.artist_name = filename.split(/[\s-.]/).first
+    this.name = filename.split(/[\s-.]/).last
+    this
+  end
+
+  def self.create_from_filename(filename = "")
+    this = self.new
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
   def save
     self.class.all << self
   end
